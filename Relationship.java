8a316c947354dd9e495221fb43055ae72ed303d8
camwhis1 <camwhis1@gmail.com>
2020-09-17 15:13:51 -0400

    Relationship Class

--- Relationship.java
+++ Relationship.java
@@ -1,4 +1,22 @@
+public class Relationship 
+{
 
-public class Relationship {
-
+	private ClassObject classOne;
+	private ClassObject classTwo;
+	
+	public Relationship(ClassObject classO, ClassObject classT)
+	{
+		classOne = classO;
+		classTwo = classT;
+	}
+	
+	public ClassObject getClassOne()
+	{
+		return classOne;
+	}
+	
+	public ClassObject getClassTwo()
+	{
+		return classTwo;
+	}
 }
