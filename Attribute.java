dafb09e9301df78d7ff4d8831ff903e6043b650b
Jeremy Craine <70176611+jbcraine@users.noreply.github.com>
2020-09-20 15:50:17 -0400

    Sprint 1  (#30)
    * Created Class-Object Class (#21)
    
    * Basic console that can quit and help (#22)
    
    * Basic console that can quit and help
    
    * Basic console that can quit and help
    
    Co-authored-by: Jeremy C <jbcraine@millersville.edu>
    
    * Relationship class (#23)
    
    * Relationship class
    
    * Relationship Class
    
    * Created WorkingProject.java (#24)
    
    Fixed bugs in ClassObject.java
    
    * Added Working Project unit tests (#25)
    
    Fixed bugs in ClassObject, WorkingProject
    
    * Why is this so hard this shouldn't be so hard fml (#27)
    
    Updated HelpDocument.txt and Console.java to have greater functionality.
    
    Co-authored-by: Jeremy C <jbcraine@millersville.edu>
    
    * Console (#28)
    
    * Why is this so hard this shouldn't be so hard fml
    Updated HelpDocument.txt and Console.java to have greater functionality.
    
    * Fixed a warning in Console.java and updated README.md
    
    Co-authored-by: Jeremy C <jbcraine@millersville.edu>
    
    * Class object (#29)
    
    * Created Class-Object Class
    
    * Fixed small issue with Class Object
    
    Co-authored-by: Jakarith <70279381+Jakarith@users.noreply.github.com>
    Co-authored-by: Jeremy C <jbcraine@millersville.edu>
    Co-authored-by: camwhis1 <70283510+camwhis1@users.noreply.github.com>
    Co-authored-by: mruchalski280 <70281593+mruchalski280@users.noreply.github.com>

---
+++ Attribute.java
@@ -0,0 +1,17 @@
+public class Attribute
+{
+   private String name;
+   public Attribute(String attrName)
+   {
+       name = attrName;
+   } 
+   public String getName()
+   {
+       return name;
+   }
+   public void setName(String newName)
+   {
+       name = newName;
+   }
+   
+}
