a302564c2043c34791fa88ceb44c17e333257e13
Ben Albright <70279381+Jakarith@users.noreply.github.com>
2020-09-27 18:27:13 -0400

    Added type support to both relationship class and working project

--- Relationship.java
+++ Relationship.java
@@ -1,13 +1,19 @@
 public class Relationship 
 {
-
+	enum relationshipType
+	{
+		COMPOSITION, AGGREGATION, GENERALIZATION, INHERITANCE;
+	}
 	private ClassObject classOne;
 	private ClassObject classTwo;
+	private relationshipType type;
 	
-	public Relationship(ClassObject classO, ClassObject classT)
+	
+	public Relationship(ClassObject classO, ClassObject classT, relationshipType t)
 	{
 		classOne = classO;
 		classTwo = classT;
+		type = t;
 	}
 	
 	public ClassObject getClassOne()
@@ -19,4 +25,15 @@ public class Relationship
 	{
 		return classTwo;
 	}
+
+	public relationshipType getRelationshipType()
+	{
+		return type;
+	}
+
+	public void setRelationshipType(relationshipType newType)
+	{
+		type = newType;
+	}
+
 }
