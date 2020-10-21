21071c85d62aaa45a889f9d052d8bc372efdef08
eahobbs <eahobbs@millersville.edu>
2020-10-20 21:19:18 -0400

    Added save file overwrite and updated relationship types

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -142,11 +142,11 @@ public class Console {
                             case "-c" : 
                                 project.addRelationship (commands.get(1), commands.get(2), "C"); 
                                 break;
-                            case "-g" : 
-                                project.addRelationship (commands.get(1), commands.get(2), "G"); 
-                                break;
                             case "-i" : 
                                 project.addRelationship (commands.get(1), commands.get(2), "I"); 
+                                break;
+                            case "-r" : 
+                                project.addRelationship (commands.get(1), commands.get(2), "R"); 
                                 break;
                             default :
                                 System.out.println("Error: no relationship type given. <class, class, type>");
@@ -367,9 +367,17 @@ public class Console {
         String filename = projectName + ".json";
         try {
             File proj = new File(filename);
-            if (proj.createNewFile())
+            if (proj.exists()) 
             {
-                FileWriter fw = new FileWriter(proj);
+                //when false file is overwritten
+                FileWriter fw = new FileWriter(proj, false);
+                fw.write(project.toJSONString());
+                fw.close();
+            }
+            else if (proj.createNewFile())
+            {
+                //when true file is appended
+                FileWriter fw = new FileWriter(proj, true);
                 fw.write(project.toJSONString());
                 fw.close();
             }
