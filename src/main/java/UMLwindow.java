0e0dde71f43d4756c722d9cb34f6bed3ab26f676
camwhis1 <camwhis1@gmail.com>
2020-10-24 14:10:27 -0400

    build.gradle

---
+++ src/main/java/UMLwindow.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.awt.GridLayout;
+
+public class UMLwindow {
+	   private JFrame win;
+	   private Menu me;
+
+	   public UMLwindow()
+	   {
+		   win = new JFrame("UML");
+		   me = new Menu();
+	       win.setLayout(new GridLayout(5,5));
+	       win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	       win.setSize(800,750);
+	       me.makeMenu(win);
+	       win.setJMenuBar(me.getMenuBar());
+	       win.setVisible(true);
+	   }
+
+	   public JFrame getMainWindow()
+	   {
+		   return win;
+	   }
+}
